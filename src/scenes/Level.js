0e4332b0e91656f5594d27b264c631bfc347b4c4
zzmfyfk <81992422+zzmfyfk@users.noreply.github.com>
2023-12-03 11:14:18 +0100

    Add files via upload

---
+++ src/scenes/Level.js
@@ -0,0 +1,46 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class Level extends Phaser.Scene {
+
+	constructor() {
+		super("Level");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		// dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying
+		const dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying = this.add.image(653, 352, "DALL·E 2023-12-03 07.06.33 - A retro pixel art style main menu screen for a classic Chinese love story themed platform game. The screen includes the title 'Liang Shanbo & Zhu Ying");
+		dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying.scaleX = 0.8489088658419323;
+		dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying.scaleY = 0.712521479827341;
+
+		this.dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying = dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying;
+
+		this.events.emit("scene-awake");
+	}
+
+	/** @type {Phaser.GameObjects.Image} */
+	dALL_E_2023_12_03_07_06_33___A_retro_pixel_art_style_main_menu_screen_for_a_classic_Chinese_love_story_themed_platform_game__The_screen_includes_the_title__Liang_Shanbo___Zhu_Ying;
+
+	/* START-USER-CODE */
+
+	// Write more your code here
+
+	create() {
+
+		this.editorCreate();
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
